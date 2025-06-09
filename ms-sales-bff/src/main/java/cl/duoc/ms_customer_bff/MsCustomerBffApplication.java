4a5f4529bcf659190ab7e8d8e07f08b48da8948e
Matias <157364130+Cofre-dev@users.noreply.github.com>
2025-06-08 21:40:17 -0400

    Añadiendo los primeros archivos
    :)

---
+++ ms-sales-bff/src/main/java/cl/duoc/ms_customer_bff/MsCustomerBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_customer_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomerBffApplication.class, args);
+	}
+
+}
